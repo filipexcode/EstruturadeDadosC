6aaac64b8795a7fcbb1b784173347bd79dd5788a
Filipe Moura <100590351+filipexcode@users.noreply.github.com>
2023-09-14 10:43:41 -0300

    Update BuscaSequencial.c

--- BuscaSequencial.c
+++ BuscaSequencial.c
@@ -6,7 +6,6 @@ int main(){
 	int i, n, vet[10] = {56, 26, 93, 17, 77, 31, 44, 55, 20, 65};
 	
 	for(i=0;i<10;i++){
-		printf("%d\t",vet[i]);
 		
 		printf("Qual o valor que voce deseja buscar: ");
 		scanf("%d",&n);
