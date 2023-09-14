d0112dc0b5648c71a532a88d48745f42883d805b
Filipe Moura <100590351+filipexcode@users.noreply.github.com>
2023-09-14 10:41:59 -0300

    Create BuscaSequencial.c

---
+++ BuscaSequencial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	int i, n, vet[10] = {56, 26, 93, 17, 77, 31, 44, 55, 20, 65};
+	
+	for(i=0;i<10;i++){
+		printf("%d\t",vet[i]);
+		
+		printf("Qual o valor que voce deseja buscar: ");
+		scanf("%d",&n);
+		
+//Implementando o metodo de busca
+	for(i=0;i<10;i++){
+		if(vet[i] == n){
+			printf("%d ocorre na posicao %d do vetor\n",n,i);
+			return 0;
+		}
+	}
+	}
+}
