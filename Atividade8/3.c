e04885763e071cf7fd7df2604185cddab9695ed4
Filipe Moura <100590351+filipexcode@users.noreply.github.com>
2023-09-20 22:11:13 -0300

    Create 3.c

---
+++ Atividade8/3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void ordenacaoPorInsercaoDecrescente(int arr[], int n) {
+    int i, j, chave;
+    for (i = 1; i < n; i++) {
+        chave = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] < chave) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = chave;
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array original: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    ordenacaoPorInsercaoDecrescente(arr, n);
+
+    printf("\nArray ordenado em ordem decrescente: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
